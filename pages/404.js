548d90af26b2a039567740969a45c697b33d1270
wingfailam <wingfai.tw@gmail.com>
2021-11-26 03:46:50 +0800

    feat: 獲取圖片的簡易 api、部署 SPA靜態網站（vue）

---
+++ pages/404.js
@@ -0,0 +1,14 @@
+import Custom4044 from "../components/4044"
+export default function Custom404() {
+
+  return (
+  <html>
+      <head>
+          {/* <Custom4044 /> */}
+      </head>
+  <body>
+    <h1>404</h1>
+  </body>
+  </html>)
+}
+
--- server.js
+++ server.js
@@ -5,9 +5,48 @@ const port = parseInt(process.env.PORT, 10) || 3000
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
+const path = require('path');
+const fs = require('fs'); 
+
+// const taiwantravel = require('./pages/taiwantravel/index.html');
 
 app.prepare().then(() => {
   const server = express()
+  server.use('/taiwantravel/', express.static(path.join(__dirname, 'public/taiwantravel')) )
+  server.use('/taiwantravel/*', express.static(path.join(__dirname, 'public/taiwantravel')) )
+  
+  server.use(express.static(path.join(__dirname, 'public')));
+// http://localhost:3000/api/projects/2019-02-12-%E5%81%89%E4%BB%94
+  server.get('/api/projects/', (req, res) => {
+    fs.readdir(__dirname + '/public/'+'projects/', (err, folders) => { 
+        if (err) 
+          console.log(err); 
+        else { 
+          folders =folders.filter(item => !(/(^|\/)\.[^\/\.]/g).test(item));
+          console.log("\nCurrent directory filenames:"); 
+          folders.forEach(file => { 
+            console.log(file); 
+          }) 
+          // folders = folders.map(el=>'http://' +req.headers.host+'/projects/' +el)
+          res.json(folders);
+        } 
+      }) 
+  })
+  server.get('/api/projects/:projectsID', (req, res) => {
+    fs.readdir(__dirname + '/public/'+'projects/' +req.params.projectsID, (err, files) => { 
+        if (err) 
+          console.log(err); 
+        else { 
+          files = files.filter(item => !(/(^|\/)\.[^\/\.]/g).test(item));
+          console.log("\nCurrent directory filenames:"); 
+          files.forEach(file => { 
+            console.log(file); 
+          }) 
+          files = files.map(el=>'http://' +req.headers.host+'/projects/' +req.params.projectsID +'/' +el)
+          res.json(files);
+        } 
+      }) 
+  })
 
   server.all('*', (req, res) => {
     return handle(req, res)
